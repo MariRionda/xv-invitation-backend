98ba2641839acc170ac403c5458c352620bbe366
Mariana Rocío Rionda <97262059+MariRionda@users.noreply.github.com>
2024-02-15 21:12:47 -0300

    cors *

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # Configurar CORS
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "https://xv-invitation-front.vercel.app",
-    "https://wedding-invitation-front.vercel.app",
-    "https://wedding-invitation-front-2.vercel.app"
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+#     "https://xv-invitation-front.vercel.app",
+#     "https://wedding-invitation-front.vercel.app",
+#     "https://wedding-invitation-front-2.vercel.app"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
